6b9f1395d4584e5a06c206d6a1568106039fde44
felixcalo <felixcalo12@hotmail.com>
2020-09-30 22:52:58 +0100

    Updates

---
+++ precache-manifest.29edaeef33913377410feab75d558ffa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a3dcc5cc1782bd48c3668218bb9719",
+    "url": "/mediCenter-ReactJS/index.html"
+  },
+  {
+    "revision": "0b4e7e23daa2299ef6ff",
+    "url": "/mediCenter-ReactJS/static/css/main.6105e34b.chunk.css"
+  },
+  {
+    "revision": "29ea5189f9dde1649161",
+    "url": "/mediCenter-ReactJS/static/js/2.0c2ab6f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mediCenter-ReactJS/static/js/2.0c2ab6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4e7e23daa2299ef6ff",
+    "url": "/mediCenter-ReactJS/static/js/main.4121a559.chunk.js"
+  },
+  {
+    "revision": "c7e9720d8d697fd3407b",
+    "url": "/mediCenter-ReactJS/static/js/runtime-main.708f2883.js"
+  },
+  {
+    "revision": "f986a302e5d70405822ae94e6c73c6d4",
+    "url": "/mediCenter-ReactJS/static/media/dr.f986a302.jpg"
+  },
+  {
+    "revision": "e6d64f35718819a419d9f1519e7a88e8",
+    "url": "/mediCenter-ReactJS/static/media/img1.e6d64f35.jpg"
+  }
+]);
